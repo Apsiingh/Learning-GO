7fc3de7fcaa655a0d29d658ca3093c469f2cd229
apsingh911 <ap.singh@techriskpartners.com>
2025-04-21 10:57:45 +0000

    Completed strcut in go

--- STRUCTS/main.go
+++ STRUCTS/main.go
@@ -5,61 +5,83 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
-	contact contactInfo
+	contact   contactInfo
 }
 
 type contactInfo struct {
-	email string
+	email   string
 	phoneNo int64
 }
 
-
-// 	FOR SINGLE STRUCT 
+// 	FOR SINGLE STRUCT
 // func main() {
-	// 1st way to declare struct value 
-	// name := person{"Akhand" ,"Pratap"}
+// 1st way to declare struct value
+// name := person{"Akhand" ,"Pratap"}
 
-	// 2nd way to declare struct value
-	// name := person{firstName: "Rahul", lastName: "Singh"}				
-	// fmt.Print(name)
+// 2nd way to declare struct value
+// name := person{firstName: "Rahul", lastName: "Singh"}
+// fmt.Print(name)
 
-	// 3rd way to declare struct value
-	// var name person 
-	// name.firstName = "komal"
-	// name.lastName = "singh"
-	// fmt.Println(name)
-	// %+v is used to print all the struct key and their value
-	// fmt.Printf("%+v",name)
+// 3rd way to declare struct value
+// var name person
+// name.firstName = "komal"
+// name.lastName = "singh"
+// fmt.Println(name)
+// %+v is used to print all the struct key and their value
+// fmt.Printf("%+v",name)
 
 // }
 
-
-//FOR INTERNAL STRUCT
+//FOR NESTED STRUCT
 func main() {
 	// Don't , forget to give commas ,
 	akhand := person{
-		firstName:"Akhand",
-		lastName:"PratapSingh",
-		contact : contactInfo{
-         email:"a.p.singh5508@gmail.com",
-	     phoneNo:123456,
+		firstName: "Akhand",
+		lastName:  "PratapSingh",
+		contact: contactInfo{
+			email:   "a.p.singh5508@gmail.com",
+			phoneNo: 123456,
 		},
 	}
 	// akhand.print()
- 
-	// &variable -- Give me the memory address of the value this 
+
+	// &variable -- Give me the memory address of the value this
 	// this variable is pointing at
-	akhandPointer := &akhand 
-	akhandPointer.updateName("Rahul")
+
+	// akhandPointer := &akhand
+	// akhandPointer.updateName("Rahul", "kumar")
+
+	akhand.updateName("rahul", "singh")
 	akhand.print()
+
+	// OVER HERE WE NOT HAVE TO SPECIFY THE POINTER IN THE SLICE
+	// BUT WHY WE HAVE TO SPECIFY POINTER  INSIDE THE STRUCT
+	// watch video no :-- 012 Reference vs Value Types
+
+	// Value Types -- Use pointers to change these things in a func
+	//  int , float , string , bool , structs
+	// Reference Types -- Don't worry about pointers with these
+	// slices , maps , channels , pointers , functions
+
+	goPlayGround()
 }
 
-
-func (pointerToPerson *person) updateName (newName string){
-    // *pointer -- Give me the value this memory address is pointing at 
-	(*pointerToPerson).firstName = newName
+func (pointerToPerson *person) updateName(firstName string, lastName string) {
+	// *pointer -- Give me the value this memory address is pointing at
+	(*pointerToPerson).firstName = firstName
+	(*pointerToPerson).lastName = lastName
 }
 
 func (p person) print() {
-	fmt.Printf("%+v",p)
+	fmt.Printf("%+v", p)
+}
+
+func goPlayGround() {
+	mySlice := []string{"Hi", "There", "How", "Are", "You"}
+	updateSlice(mySlice)
+}
+
+func updateSlice(s []string) {
+	s[0] = "Bye"
+	fmt.Println(s)
 }
